1796b42232abf21b580ccfc466e6ea6964662c5a
Ahmet Faruk Yılmaz <farukylm34@gmail.com>
2023-01-09 00:39:44 +0300

    Add y-presence

--- frontend/src/ydoc.js
+++ frontend/src/ydoc.js
@@ -2,6 +2,10 @@ import { WebrtcProvider } from "y-webrtc";
 import { Doc } from "yjs";
 
 const ydoc = new Doc();
-new WebrtcProvider("yjs-demo", ydoc);
+const provider = new WebrtcProvider("yjs-demo", ydoc);
+
+export const awareness = provider.awareness;
+
+awareness.setLocalState({ name: "John Doe", email: "johndoe@gmail.com" });
 
 export default ydoc;
