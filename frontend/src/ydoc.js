d717ae2d6cc6d64c277de4cf2bffa3e9ed71cf5e
Ahmet Faruk Yılmaz <farukylm34@gmail.com>
2023-01-12 22:16:26 +0300

    Update signalling server

--- frontend/src/ydoc.js
+++ frontend/src/ydoc.js
@@ -2,6 +2,8 @@ import { WebrtcProvider } from "y-webrtc";
 import { Doc } from "yjs";
 
 const ydoc = new Doc();
-new WebrtcProvider("yjs-demo", ydoc);
+new WebrtcProvider("yjs-demo", ydoc, {
+  signalling: ["wss://simplifyql-m49v.vercel.app"],
+});
 
 export default ydoc;
