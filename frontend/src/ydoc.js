dfbe80fa8922261429bff716fdb288a6ab4ab1aa
Ahmet Faruk Yılmaz <farukylm34@gmail.com>
2023-01-12 22:30:53 +0300

    Revert ydoc changes

--- frontend/src/ydoc.js
+++ frontend/src/ydoc.js
@@ -2,12 +2,6 @@ import { WebrtcProvider } from "y-webrtc";
 import { Doc } from "yjs";
 
 const ydoc = new Doc();
-new WebrtcProvider("yjs-demo", ydoc, {
-  signaling: [
-    "wss://simplifyql-m49v.vercel.app",
-    "wss://demos.yjs.dev",
-    "wss://simplifyql.vercel.app",
-  ],
-});
+new WebrtcProvider("yjs-demo", ydoc);
 
 export default ydoc;
