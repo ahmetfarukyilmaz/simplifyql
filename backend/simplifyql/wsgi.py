4f4a8e130eda18fb465f83000b0cc7f3fc2f2934
Ahmet Faruk Yılmaz <farukylm34@gmail.com>
2023-01-12 22:06:37 +0300

    Update wsgi

--- backend/simplifyql/wsgi.py
+++ backend/simplifyql/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simplifyql.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "simplifyql.settings")
 
-application = get_wsgi_application()
+app = get_wsgi_application()
