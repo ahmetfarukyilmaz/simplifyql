6c9ce7fe1e6258249c10791e1efa7c53456a6aae
Ahmet Faruk Yılmaz <farukylm34@gmail.com>
2023-01-12 21:59:59 +0300

    Update vercel settings

--- backend/simplifyql/settings.py
+++ backend/simplifyql/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 import sys
 from pathlib import Path
 
@@ -139,7 +140,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
+STATICFILES_DIRS = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
